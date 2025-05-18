7f5be71d66899b74a168b0048ba539d3172da0ce
Jaehyeon Kim <dottami@gmail.com>
2025-05-19 08:46:01 +1000

    delete old apps

--- kotlin-examples/kafka-json-clients/src/main/kotlin/me/jaehyeon/Main.kt
+++
@@ -1,9 +0,0 @@
-package me.jaehyeon
-
-fun main(args: Array<String>) {
-    when (args.getOrNull(0)?.lowercase()) {
-        "producer" -> KafkaProducerApp.run()
-        "consumer" -> KafkaConsumerApp.run()
-        else -> println("Usage: <producer|consumer>")
-    }
-}
--- kotlin-examples/kafka-json-clients/src/main/kotlin/me/jaehyeon/model/User.kt
+++
@@ -1,7 +0,0 @@
-package me.jaehyeon.model
-
-data class User(
-    val name: String,
-    val email: String,
-    val age: Int,
-)
